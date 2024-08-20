5fcd67a6cb7dedf8b90ea3c49de28789c843b353
VictorL17 <leonardovictor1706@gmail.com>
2024-08-20 18:55:25 -0300

    Tela do edittext

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,25 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val edittextPeso = findViewById<TextInputEditText>(R.id.edittext_peso)
+        val edittextAltura = findViewById<TextInputEditText>(R.id.edittext_altura)
+
+        val btn_Calcular = findViewById<Button>(R.id.btn_Calcular)
+
+        btn_Calcular.setOnClickListener {
+           val peso = edittextPeso.text
+            val altura = edittextAltura.text
+           println("Victor acao do botao" + altura)
+        }
+
     }
 }
